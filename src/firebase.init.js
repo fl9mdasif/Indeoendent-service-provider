56c6a9380732d7db0357304d553b5bf4f854ce54
fl9mdasif <freelancer.mdasifofficial@gmail.com>
2022-04-18 20:21:48 +0600

    install react firebse hook

---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAOGC5dODK-9cByDWkVPO3nusQSFX_0epw",
+  authDomain: "capturedmoment.firebaseapp.com",
+  projectId: "capturedmoment",
+  storageBucket: "capturedmoment.appspot.com",
+  messagingSenderId: "1043378845993",
+  appId: "1:1043378845993:web:77cf65b0360e4f133f9f38"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth (app);
